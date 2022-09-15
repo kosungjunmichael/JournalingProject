dda323eaa7945b3905fbdbddca3888ef13b26ae8
Alex <akim384@gmail.com>
2022-09-15 15:30:09 +0900

    upadates

--- view/albumView.php
+++ view/albumView.php
@@ -3,7 +3,36 @@
 <?php ob_start();?>
 <?php include("sidebarView.php");?>
 
-<div id="album"></div>
+<h1>Album</h1>
+<div id="album-container">
+    <div class="pic-place">
+        <img src="https://source.unsplash.com/1600x900/?people" alt="">
+    </div>
+    <div class="pic-place">
+        <img src="">
+    </div>
+    <div class="pic-place">
+        <img src="">
+    </div>
+    <div class="pic-place">
+        <img src="">
+    </div>
+    <div class="pic-place">
+        <img src="">
+    </div>
+    <div class="pic-place">
+        <img src="">
+    </div>
+    <div class="pic-place">
+        <img src="">
+    </div>
+    <div class="pic-place">
+        <img src="">
+    </div>
+    <div class="pic-place">
+        <img src="">
+    </div>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 <?php require('templateView.php'); ?>
--- view/timelineView.php
+++ view/timelineView.php
@@ -1,16 +1,84 @@
 <?php $title = "Timeline";?>
+<?php $style = "timeline";?>
 <?php ob_start();?>
+<?php include("sidebarView.php");?>
 
-<h1>Timeline</h1>
 <?php
     if (isset($_REQUEST['type']) && $_REQUEST['type'] === 'registered') {
         // this should be turn into a little notification modal thing
         echo "<p>You have been registered. Welcome!</p>";
     }
+    ?>
 
-//        session_start();
-//        print_r($_SESSION);
-?>
+<div id="timeline">
+    
+    <h1>Timeline</h1>
+    
+    <h2>Month</h2>
+    <div class="month1">
+        <div class="container">
+            <h3>title of the entry</h3>
+            <p>content of the entry</p>
+            <h5>hashtangs</h5>
+            <h5>date</h5>
+        </div>
+        <div class="container">
+            <h3>title of the entry</h3>
+            <p>content of the entry</p>
+            <h5>hashtangs</h5>
+            <h5>date</h5>
+        </div>
+        <div class="container">
+            <h3>title of the entry</h3>
+            <p>content of the entry</p>
+            <h5>hashtangs</h5>
+            <h5>date</h5>
+        </div>
+    </div>
+
+    <h2>Month</h2>
+    <div class="month2">
+        <div class="container">
+            <h3>title of the entry</h3>
+            <p>content of the entry</p>
+            <h5>hashtangs</h5>
+            <h5>date</h5>
+        </div>
+        <div class="container">
+            <h3>title of the entry</h3>
+            <p>content of the entry</p>
+            <h5>hashtangs</h5>
+            <h5>date</h5>
+        </div>
+        <div class="container">
+            <h3>title of the entry</h3>
+            <p>content of the entry</p>
+            <h5>hashtangs</h5>
+            <h5>date</h5>
+        </div>
+    </div>
+    
+    <h2>Month</h2>
+    <div class="month3">
+        <div class="container">
+            <h3>title of the entry</h3>
+            <p>content of the entry</p>
+            <h5>hashtangs</h5>
+            <h5>date</h5></div>
+        <div class="container">
+            <h3>title of the entry</h3>
+            <p>content of the entry</p>
+            <h5>hashtangs</h5>
+            <h5>date</h5>
+        </div>
+        <div class="container">
+            <h3>title of the entry</h3>
+            <p>content of the entry</p>
+            <h5>hashtangs</h5>
+            <h5>date</h5>
+        </div>
+    </div>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 <?php require('templateView.php'); ?>
