8436151987945a7a877e7a9e6bb7d713843b936b
James Kim <jameskim917@gmail.com>
2022-10-03 21:06:09 +0900

    calendar page feature functional

--- controller/api/getEntries.php
+++ controller/api/getEntries.php
@@ -1,22 +1,15 @@
 <?php
 // API endpoint to get entry locations
+session_start();
 // set uid from session variable
 if (isset($_SESSION["uid"])) {
 	$uid = $_SESSION["uid"];
 
-	// get data from db
+	// get entries data from db
 	require_once "../../model/EntryManager.php";
 	$entryManager = new EntryManager();
 	$entries = $entryManager->getEntries($uid, "all");
 
-	//    $locations = array();
-
-	//    foreach ($entries as $entry) {
-	//        $locations[] = array(
-	//            'u_id' => $entry['u_id'],
-	//            'location' => $entry['location']
-	//        );
-	//    }
 	header("Content-Type: application/json; charset=utf-8");
 	echo json_encode($entries);
 }
