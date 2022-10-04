df5f85a9cff0d7ed32d50b1b9307ab47e659f4be
kosungjunmichael <mike.ko96@gmail.com>
2022-10-04 17:19:58 +0900

    working on combining sql queries

--- model/EntryManager.php
+++ model/EntryManager.php
@@ -143,30 +143,16 @@ class EntryManager extends Manager
 			"userId" => $userId,
 		]);
 		if ($entryGroup === "all") {
+            // return $entryContents = $this->getEnt("allEntries", $userId);
 			return $req->fetchAll(PDO::FETCH_ASSOC);
 		} else {
 			// empty array to store the return content
 			$entriesDisplay = [];
-			while ($entryContent = $req->fetch(PDO::FETCH_ASSOC)) {
+            $entryContents = $this->getEnt("allEntries", $userId);
+            foreach($entryContents as $entryContent){
+            
 				// if Monthly
 				if ($entryGroup === "monthly") {
-					// for current year
-					//					if ($entryContent["year"] == $thisYear) {
-					// check if the keyname exists in the $entriesDisplay
-					//						if (array_key_exists($entryContent["month"], $entriesDisplay)) {
-					//							// push the entryContent into the key
-					//							array_push(
-					//								$entriesDisplay[$entryContent["month"]],
-					//								$entryContent
-					//							);
-					//						} else {
-					//							// create the array in the key & push the entryContent into the key
-					//							$entriesDisplay[$entryContent["month"]] = [];
-					//							array_push(
-					//								$entriesDisplay[$entryContent["month"]],
-					//								$entryContent
-					//							);
-					//						}
 					$monthYearKey = $entryContent["month"] . " " . $entryContent["year"];
 					// check if the keyname exists in the $entriesDisplay
 					if (array_key_exists($monthYearKey, $entriesDisplay)) {
@@ -211,29 +197,8 @@ class EntryManager extends Manager
 	public function getEntry($entryId, $userId)
 	{
 		$db = $this->dbConnect();
-		$req = $db->prepare('SELECT e.title
-        , e.u_id
-        , e.text_content
-        , e.location
-        , e.weather
-        , e.last_edited
-        , e.date_created
-        , DAY(e.last_edited) as day
-        , MONTHNAME(e.last_edited) as month
-        , YEAR(e.last_edited) as year
-        , TIME_FORMAT(e.last_edited, "%h:%i %p") as time
-        , GROUP_CONCAT(t.tag_name) as tags
-        FROM entries e
-        LEFT JOIN tag_map tm ON e.u_id = tm.entry_id
-        LEFT JOIN tags t ON t.id = tm.tag_id
-        WHERE e.user_uid = :userId 
-        AND e.u_id = :entryId 
-        AND e.is_active = 1');
-		$req->execute([
-			"userId" => $userId,
-			"entryId" => $entryId,
-		]);
-		$entryContent = $req->fetch(PDO::FETCH_ASSOC);
+		
+        $entryContent = $this->getEnt("singleEntry", $userId, $entryId);
 
 		$imagesReq = $db->prepare(
 			"SELECT path FROM entry_images WHERE entry_uid = ?"
@@ -243,7 +208,7 @@ class EntryManager extends Manager
 		$entryContent["images"] = $images;
 
 		return $entryContent;
-		$req->closeCursor();
+		$imagesReq->closeCursor();
 	}
 
     public function deleteEntry($entryUId, $userUID)
--- model/Manager.php
+++ model/Manager.php
@@ -11,6 +11,69 @@ class Manager
 		);
 	}
 
+    protected function getEnt($request, $userUID, $entryUID = null)
+    {
+        $db = $this->dbConnect();
+
+        switch ($request){
+            case "allEntries":
+                $req = $db->prepare('SELECT
+                e.u_id
+                , e.title
+                , e.location
+                , e.lat_lng
+                , e.text_content
+                , e.date_created
+                , e.last_edited
+                , DAYNAME(e.last_edited) as dayname
+                , DAY(e.last_edited) as day
+                , WEEK(e.last_edited) as week
+                , MONTHNAME(e.last_edited) as month
+                , YEAR(e.last_edited) as year
+                , GROUP_CONCAT(t.tag_name) as tags
+                -- add GROUP_CONCAT function here. give it an alias like "as tags" 
+                FROM entries e
+                LEFT JOIN tag_map tm ON e.u_id = tm.entry_id
+                LEFT JOIN tags t ON t.id = tm.tag_id
+                WHERE e.user_uid = :userId
+                AND e.is_active = 1
+                GROUP BY last_edited DESC');
+                $req->execute([
+                    "userId" => $userUID
+                ]);
+                return $req->fetchAll(PDO::FETCH_ASSOC);
+            break;
+            case "singleEntry":
+                $req = $db->prepare('SELECT e.title
+                , e.u_id
+                , e.text_content
+                , e.location
+                , e.weather
+                , e.last_edited
+                , e.date_created
+                , DAY(e.last_edited) as day
+                , MONTHNAME(e.last_edited) as month
+                , YEAR(e.last_edited) as year
+                , TIME_FORMAT(e.last_edited, "%h:%i %p") as time
+                , GROUP_CONCAT(t.tag_name) as tags
+                FROM entries e
+                LEFT JOIN tag_map tm ON e.u_id = tm.entry_id
+                LEFT JOIN tags t ON t.id = tm.tag_id
+                WHERE e.user_uid = :userId 
+                AND e.u_id = :entryId 
+                AND e.is_active = 1');
+                $req->execute([
+                    "userId" => $userUID,
+                    "entryId" => $entryUID,
+                ]);
+                return $req->fetch(PDO::FETCH_ASSOC);
+            break;
+            default:
+            break;
+        }
+
+    }
+
 	protected function uidCreate()
 	{
 		if (!function_exists("crypto_rand_secure")) {
