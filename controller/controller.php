bdcdadfcf56b7924247cbdb0c3cac27c0355decb
kosungjunmichael <mike.ko96@gmail.com>
2022-10-06 10:11:56 +0900

    copied code for timeline from old branch

--- controller/controller.php
+++ controller/controller.php
@@ -18,31 +18,36 @@
  	require ROOT . "/view/aboutView.php";
  }
 
- function toTimeline($alertText)
- {
- 	$entry_manager = new EntryManager();
- 	$user_manager = new UserManager();
- 	$entries = $entry_manager->getEntries($_SESSION["uid"], "monthly");
- 	if (isset($alertText["alert"])) {
- 		switch ($alertText["alert"]) {
- 			case "newEntry":
- 				$alert = "Entry successfully created";
- 				break;
- 			case "deleteEntry":
- 				$alert = "Entry Successfully deleted";
- 				break;
- 			case "login":
- 				$username = $user_manager->getUsername($_SESSION["uid"])[0];
- 				$alert = "Welcome back! $username";
- 				break;
- 			default:
- 				break;
- 		}
- 	}
- 	$view = "monthly";
+ function toTimeline($alertText,$entryGroup)
+{
+    $entry_manager = new EntryManager();
+    $user_manager = new UserManager();
+    if (isset($alertText['alert'])){
+        switch ($alertText['alert']){
+            case "newEntry":
+                $alert = "Entry successfully created";
+            break;
+            case "deleteEntry":
+                $alert = "Entry Successfully deleted";
+                break;
+            case "login":
+                $username = $user_manager->getUsername($_SESSION['uid'])[0];
+                $alert = "Welcome back! $username";
+            break;
+            default:
+            break;
+        }
+    }
+    if ($entryGroup === "monthly"){
+        $entries = $entry_manager->getEntries($_SESSION["uid"], "monthly");
+        // echoPre($entries);
+    } else if ($entryGroup === "weekly"){
+        $entries = $entry_manager->getEntries($_SESSION["uid"], "weekly");
+    }
+    $view = $entryGroup;
 
- 	require ROOT . "/view/timelineView.php";
- }
+    require ROOT . "/view/timelineView.php";
+}
 
  function toAlbum($u_id)
  {
